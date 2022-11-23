7009b94b37d137c91ac11d2203d721e202b337d8
Jan Willem Henckel <farly.de@gmail.com>
2022-11-23 12:00:37 +0100

    add next and prev id

--- api/bad-jokes/[id].ts
+++ api/bad-jokes/[id].ts
@@ -3,11 +3,15 @@ import jokes from '../../data/bad-jokes';
 
 export default (request: VercelRequest, response: VercelResponse) => {
   const id = Number.parseInt(request.query.id as string, 10);
-  const joke = jokes.find(joke => joke.id === id);
+  const jokeIndex = jokes.findIndex(joke => joke.id === id);
+  const joke = jokes[jokeIndex];
 
   if (!joke) {
     return response.status(404).json({ status: 'Not Found' });
   }
 
-  response.status(200).json(joke);
+  const nextId = jokes[jokeIndex + 1]?.id || jokes[0].id;
+  const prevId = jokes[jokeIndex - 1]?.id || jokes[jokes.length - 1].id;
+
+  response.status(200).json({ ...joke, nextId, prevId });
 };
