3821462b4fbe36e713334e59d779e8156b59c7be
Jan Willem Henckel <jan@mpfilm.de>
2022-12-13 15:02:50 +0100

    Add Art Endpoint (#1)
    Co-authored-by: Jan Willem Henckel <jan@mpfilm.de>
    Co-authored-by: PReichetanz <paul.reichetanz@gmx.de>
    Co-authored-by: Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>

---
+++ api/art/index.ts
@@ -0,0 +1,17 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import path from 'node:path';
+import artworks from '../../data/art';
+
+export default (request: VercelRequest, response: VercelResponse) => {
+  const artworksWithUpdatedImageSource = artworks.map(artwork => {
+    return {
+      ...artwork,
+      imageSource: `https://${path.join(
+        process.env.VERCEL_URL || '',
+        artwork.imageSource
+      )}`,
+    };
+  });
+
+  response.status(200).json(artworksWithUpdatedImageSource);
+};
