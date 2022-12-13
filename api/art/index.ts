e6b632f8c445d6ae4fcb04723ae9eaf53cc21d69
PReichetanz <paul.reichetanz@gmx.de>
2022-12-13 14:56:11 +0100

    fix art api after review

--- api/art/index.ts
+++ api/art/index.ts
@@ -3,14 +3,17 @@ import path from 'path';
 import artworks from '../../data/art';
 
 export default (request: VercelRequest, response: VercelResponse) => {
+  console.log(process.env.VERCEL_URL);
   const artworksWithUpdatedImageSource = artworks.map(artwork => {
+    console.log(artwork.imageSource);
     return {
       ...artwork,
-      imageSource: path.join(process.env.VERCEL_URL || '', artwork.imageSource),
+      imageSource: `https://${path.join(
+        process.env.VERCEL_URL || '',
+        artwork.imageSource
+      )}`,
     };
   });
 
-  response.status(200).json({
-    data: artworksWithUpdatedImageSource,
-  });
+  response.status(200).json(artworksWithUpdatedImageSource);
 };
