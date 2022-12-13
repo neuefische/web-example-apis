8c9806b31ca9583f4b41acde12ca5245bb36c8ae
Jan Willem Henckel <farly.de@gmail.com>
2022-12-13 15:59:18 +0100

    rename variable

--- api/art/index.ts
+++ api/art/index.ts
@@ -2,13 +2,13 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import path from 'node:path';
 import artworks from '../../data/art';
 
-const baseUrl = process.env.HOSTNAME ?? process.env.VERCEL_URL!;
+const hostname = process.env.HOSTNAME ?? process.env.VERCEL_URL!;
 
 export default (request: VercelRequest, response: VercelResponse) => {
   const artworksWithUpdatedImageSource = artworks.map(artwork => {
     return {
       ...artwork,
-      imageSource: `https://${path.join(baseUrl, artwork.imageSource)}`,
+      imageSource: `https://${path.join(hostname, artwork.imageSource)}`,
     };
   });
 
